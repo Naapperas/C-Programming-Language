269804592ef1087fca465c5e8870c94b5a3985ac
Nuno Pereira <nunoafonso2002@gmail.com>
2021-01-19 19:48:40 +0000

    Hello world in C

---
+++ hello_world/hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // include the standard I/O library
+
+int main() // the main function to be called
+{
+    printf("Hello World!\n"); // print the string
+    return 0; //must include a return type, since main is an int function 
+}
