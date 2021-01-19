f85d079eb3457ec9566b396dded4eef2ae5f5189
Nuno Pereira <nunoafonso2002@gmail.com>
2021-01-19 19:56:58 +0000

    Completed exercise 1-2 from the book.

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -5,3 +5,21 @@ int main() // the main function to be called
     printf("Hello World!\n"); // print the string
     return 0; //must include a return type, since main is an int function 
 }
+
+/*
+Exercise 1-2:
+
+"Experiment to find out what happens when you add \c, where c is a character not included in the ones listed above."
+
+int main() // the main function to be called
+{
+    printf("Hello World!yn"); // print the string
+    return 0; //must include a return type, since main is an int function 
+}
+
+hello_world.c: In function ‘main’:
+hello_world.c:5:12: warning: unknown escape sequence: '\y'
+    5 |     printf("Hello World!\y"); // print the string
+      |            ^~~~~~~~~~~~~~~~
+
+*/
