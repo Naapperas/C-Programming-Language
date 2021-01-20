80e1127e3cf083d5eff3b8ca095c98b714fefa1c
Nuno Pereira <nunoafonso2002@gmail.com>
2021-01-20 10:41:51 +0000

    Added exercise 1-4 from the book

---
+++ vars_arith_expr/exercises/c_1-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    float fahr;
+    int celsius, start, stop, step;
+
+    start = 0;
+    stop = 300;
+    step = 2;
+
+    celsius = step;
+    while(celsius <= stop){
+        fahr = (9.0/5.0) * celsius + 32;
+        printf("%d\t%f\n", celsius, fahr);
+        celsius += step;
+    }
+
+}
