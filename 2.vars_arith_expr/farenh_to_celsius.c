55f5a8b68362bb07833a63a8a9799a0b1e0044e8
Nuno Pereira <nunoafonso2002@gmail.com>
2021-01-27 17:06:24 +0000

    Added symbolic constants

--- 2.vars_arith_expr/farenh_to_celsius.c
+++ 2.vars_arith_expr/farenh_to_celsius.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 2
+
+int main() // floating point version, more precise, using symbolic constants
+{
+    float fahr, celsius; // variables must be declared before they are used
+
+    fahr = LOWER;
+    while(fahr <= UPPER){ // while loop, 'while' the condition is met, execute the code inside brackets
+        celsius = (5.0 / 9.0) * (fahr - 32); // integer division truncates: this means that 5/9 would become 0, making all celsius values efectively 0. Here we can to the division since we are working in floating point arithmetic
+        printf("%3.0f %6.1f\n", fahr, celsius); // %f specifies floating point argument, \t inserts a tab. Specifying a number on the % part of the format string specfies the minimum length to print the argument
+        fahr += STEP;
+    }
+    
+    return 0;
+}
+
+/*
 int main() // floating point version, more precise
 {
     float fahr, celsius; // variables must be declared before they are used
@@ -18,7 +37,7 @@ int main() // floating point version, more precise
     
     return 0;
 }
-
+*/
 /* Integer version of the program, not accurate for temperatures
 int main()
 {
